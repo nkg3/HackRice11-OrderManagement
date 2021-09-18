854958f8026be2691df34fa4cb07e7bc1340763b
nkg3 <65669737+nkg3@users.noreply.github.com>
2021-09-18 15:43:21 -0500

    Order form frontend, navbar

--- src/FlaskApp/dashboard.py
+++ src/FlaskApp/dashboard.py
@@ -6,6 +6,31 @@ from flask import (
 
 bp = Blueprint('dash', __name__, url_prefix='/dashboard')
 
+# Dashboard home
 @bp.route("/")
-def hello():
+def home():
     return render_template('dashboard/dashboard.html')
+
+
+# Add new work order
+@bp.route("/addOrder")
+def add_order():
+    return render_template('dashboard/orderform.html')
+
+
+# Monitor Routes
+@bp.route("/orders")
+def orders():
+    return render_template('dashboard/orders.html')
+
+@bp.route("/workers")
+def workers():
+    return render_template('dashboard/workers.html')
+
+@bp.route("/facilities")
+def facilities():
+    return render_template('dashboard/facilities.html')
+
+@bp.route("/equipment")
+def equipment():
+    return render_template('dashboard/equipment.html')
