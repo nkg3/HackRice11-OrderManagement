548453ab642b0c7e492a5993946905bd8d59cf20
bill-rui <74115078+bill-rui@users.noreply.github.com>
2021-09-18 10:51:40 -0500

    comments added for reset_database script

--- src/Database/reset_database.py
+++ src/Database/reset_database.py
@@ -3,15 +3,17 @@ import database
 
 """
 Clear database and parse the entire excel file into database
+set the url, credentials, and excel files
+use database.set_with_excel(<excel path>, <sheet name>, <row range (tuple)>, <col rage (tuple)>, <database dir reference>)
 """
 
 if __name__ == "__main__":
     database_url = 'https://hackrice11-ordermanageme-327b0-default-rtdb.firebaseio.com/'
-    cred_path = Path("../DataFiles/database_private_key.json")
-    excel_path = Path("../DataFiles/cheveron_data_file.xlsx")
+    cred_path = Path("../DataFiles/database_private_key.json")  # path to database credential
+    excel_path = Path("../DataFiles/cheveron_data_file.xlsx")  # path to excel sheets
 
     database.init_database(database_url, cred_path)
-    database.set_with_dict({}, "/")
+    database.set_with_dict({}, "/")  # clear database
     database.set_with_excel(excel_path, "Equipment Details", (2, 12), (2, 9), "/equipment")
     database.set_with_excel(excel_path, "Worker Details", (1, 11), (2, 6), "/worker")
     database.set_with_excel(excel_path, "Facility Details", (2, 7), (2, 5), "/facility")
